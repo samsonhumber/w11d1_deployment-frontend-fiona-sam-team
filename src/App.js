c1cf8976843007c1dcda91d80ac20dfa9ad9b0ed
samsonhumber <printerfissh693@gmail.com>
2022-07-04 15:55:51 +0100

    see if another trivial commit does anything

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World! We are Fiona and Sam!
+          Hello World! We are Fiona and Sam! And we are having trouble with env variables.
         </a>
       </header>
     </div>
