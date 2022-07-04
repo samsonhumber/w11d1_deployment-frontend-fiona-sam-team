01ebe0cae198eb0111e39da6f1327bb01f410d6e
samsonhumber <printerfissh693@gmail.com>
2022-07-04 16:23:07 +0100

    set up form on react

--- src/App.js
+++ src/App.js
@@ -9,14 +9,27 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and {process.env.REACT_APP_WELCOME} {process.env.REACT_APP_SUBTITLE}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World! We are Fiona and Sam! And we are having trouble with env variables.
-        </a>
+        <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact"/>
+    <p>
+      <label>Your Name: <input type="text" name="name" /></label>
+    </p>
+    <p>
+      <label>Your Email: <input type="email" name="email" /></label>
+    </p>
+    <p>
+      <label>Your Role: <select name="role[]" multiple>
+        <option value="leader">Leader</option>
+        <option value="follower">Follower</option>
+      </select></label>
+    </p>
+    <p>
+      <label>Message: <textarea name="message"></textarea></label>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+</form>
       </header>
     </div>
   );
