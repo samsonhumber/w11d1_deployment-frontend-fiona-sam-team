f86a4cf8596246f5f8a8ef6399b4363f62100dda
samsonhumber <printerfissh693@gmail.com>
2022-07-05 08:49:42 +0100

    simplify form a bit hopefully

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import NetlifyForm from './netlifyForm';
 
 function App() {
   return (
@@ -9,27 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and {process.env.REACT_APP_WELCOME} {process.env.REACT_APP_SUBTITLE}
         </p>
-        <form name="contact" method="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact"/>
-    <p>
-      <label>Your Name: <input type="text" name="name" /></label>
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Your Role: <select name="role[]" multiple>
-        <option value="leader">Leader</option>
-        <option value="follower">Follower</option>
-      </select></label>
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <input type="submit" value="Submit form"></input>
-    </p>
-</form>
+      <NetlifyForm/>
       </header>
     </div>
   );
---
+++ src/netlifyForm/index.js
@@ -0,0 +1,28 @@
+export default function NetlifyForm() {
+   return (
+   <form name="contact" method="POST" data-netlify="true">
+    <input type="hidden" name="form-name" value="contact"/>
+    <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+    </p>
+    <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+    </p>
+    </form>) 
+}
+
+
+/*
+<p>
+      <label>Your Role: <select name="role[]" multiple>
+        <option value="leader">Leader</option>
+        <option value="follower">Follower</option>
+      </select></label>
+    </p>
+    <p>
+      <label>Message: <textarea name="message"></textarea></label>
+    </p>
+    <p>
+      <input type="submit" value="Submit form"></input>
+    </p>
+    */
